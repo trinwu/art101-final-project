de88218960bfd4283f85472a090ec5a6433ca4ed
trinwu <trini.me28@gmail.com>
2024-12-02 15:49:31 -0800

    adding species, sightings, checklist tables

--- apps/birds/models.py
+++ apps/birds/models.py
@@ -20,4 +20,27 @@ def get_time():
 #
 ## always commit your models to avoid problems later
 
+db.define_table(
+    'species',
+    Field('COMMON_NAME', 'string')
+)
+
+db.define_table(
+    'sightings',
+    Field('SAMPLING_EVENT_IDENTIFIER', 'string'),
+    Field('COMMON_NAME', 'string'),
+    Field('OBSERVATION_COUNT', 'string')
+)
+
+db.define_table(
+    'checklist',
+    Field('SAMPLING_EVENT_IDENTIFIER', 'string'),
+    Field('LATITUDE', 'double'),
+    Field('LONGITUDE', 'double'),
+    Field('OBSERVATION_DATE', 'date'),
+    Field('TIME_OBSERVATIONS_STARTED', 'time'),
+    Field('OBSERVER_ID', 'string'),
+    Field('DURATION_MINUTES', 'double')
+)
+
 db.commit()
