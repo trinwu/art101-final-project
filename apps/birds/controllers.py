fa3b2ae77b0285dd5dbb892cf824dd0305fc0b9c
Pallavi Rajeev <pallavirajeev44@gmail.com>
2024-12-10 17:36:43 -0800

    starting to authorize user

--- apps/birds/controllers.py
+++ apps/birds/controllers.py
@@ -78,11 +78,15 @@ def get_checklists():
 @action('add_checklist')
 @action.uses('add_checklist.html', db, auth)
 def add_checklist():
+    if not auth.current_user:
+        redirect(URL('auth/login'))
     return dict()
 
 @action('my_checklists')
 @action.uses('my_checklists.html', db, auth)
 def my_checklists():
+    if not auth.current_user:
+        redirect(URL('auth/login'))
     return dict()
 
 @action('stats')
